0814cd2464f917acc12bc832764abd45cffce369
Meik Specht <meik@schnitzelspecht.de>
2015-09-03 00:55:16 +0200

    fixed indentation

--- bin/cmd.js
+++ bin/cmd.js
@@ -17,7 +17,7 @@ var tmpdir = (os.tmpdir || os.tmpDir)();
 var Client = null, Invoice = null, TrackedTime = null;
 
 var argv = minimist(process.argv.slice(2), {
-alias: { v: 'verbose', h: 'help' }
+	alias: { v: 'verbose', h: 'help' }
 });
 
 // create data directory
@@ -34,43 +34,43 @@ var db = new Seq('main', null, null, { host: 'localhost', dialect: 'sqlite', sto
 
 // check if db is already initialized and do so if not
 if (! nconf.get('init')) {
-  console.log('DB not initialized! Initializing now...');
+	console.log('DB not initialized! Initializing now...');
 
-  // ########## model definition here! #####
-  var Client = db.define('Client', {
-    name: Seq.STRING,
-    street1: Seq.STRING,
-    street2: Seq.STRING,
-    zip: Seq.STRING(7),
-    city: Seq.STRING
-  });
+	// ########## model definition here! #####
+	var Client = db.define('Client', {
+		name: Seq.STRING,
+		street1: Seq.STRING,
+		street2: Seq.STRING,
+		zip: Seq.STRING(7),
+		city: Seq.STRING
+	});
 
-  var Invoice = db.define('Invoice', {
-    date: Seq.DATE
-  });
+	var Invoice = db.define('Invoice', {
+		date: Seq.DATE
+	});
 
-  // set relations
-  Invoice.hasMany(Client);
+	// set relations
+	Invoice.hasMany(Client);
 
-  var TrackedTime = db.define('TrackedTime', {
-    start: Seq.DATE,
-    end: Seq.DATE,
-    title: Seq.STRING,
-    description: Seq.TEXT
-  });
+	var TrackedTime = db.define('TrackedTime', {
+		start: Seq.DATE,
+		end: Seq.DATE,
+		title: Seq.STRING,
+		description: Seq.TEXT
+	});
 
-  // set relations
-  TrackedTime.hasMany(Client);
+	// set relations
+	TrackedTime.hasMany(Client);
 
-  // ########## model definition end!  #####
+	// ########## model definition end!  #####
 
-  //TODO: specify relations
-  //INFO: http://www.redotheweb.com/2013/02/20/sequelize-the-javascript-orm-in-practice.html
+	//TODO: specify relations
+	//INFO: http://www.redotheweb.com/2013/02/20/sequelize-the-javascript-orm-in-practice.html
 
-  //TODO: initialize DB if not done already
+	//TODO: initialize DB if not done already
 
-  // save changes to db:
-  db.sync();
+	// save changes to db:
+	db.sync();
 }
 
 console.log('saving some clients now...');
@@ -90,36 +90,36 @@ Client.destroy({where: {id: {$gt: 2}}});
 console.log('saved client data!\n');
 
 Client.findAll().then(function (clients) {
-  if (typeof clients == 'undefined' || ! clients) {
-    console.log('error fetching clients!\n');
-    return -1;
-  }
-  clients.forEach(function(client) {
-    console.log('found client: ' + client.name);
-    console.log('address: ' + client.street1 + " - " + client.zip + " " + client.city);
-    console.log('---------------------------------');
-  });
+	if (typeof clients == 'undefined' || ! clients) {
+		console.log('error fetching clients!\n');
+		return -1;
+	}
+	clients.forEach(function(client) {
+		console.log('found client: ' + client.name);
+		console.log('address: ' + client.street1 + " - " + client.zip + " " + client.city);
+		console.log('---------------------------------');
+	});
 });
 
 // process user commands
 if (argv.h) {
-  usage(0)
+	usage(0)
 }
 else if (argv._[0] === 'start') {
-  var d = argv.date ? new Date(argv.date) : new Date;
-  //TODO: start!
+	var d = argv.date ? new Date(argv.date) : new Date;
+	//TODO: start!
 }
 
 function saveConfig() {
-  nconf.save(function (err) {
-      if (err) {
-      console.error(err.message);
-      return;
-      }
-      console.log('Configuration saved successfully.');
-      });
+	nconf.save(function (err) {
+		if (err) {
+			console.error(err.message);
+			return;
+		}
+		console.log('Configuration saved successfully.');
+	});
 }
 
 function usage(arg) {
-  console.log("TODO!");
+	console.log("TODO!");
 }
