0344a64ce8e480af3f67b7e2f0dcb82db6a72dc9
Meik Specht <meik@schnitzelspecht.de>
2015-09-07 22:58:26 +0200

    finished initDb function # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # On branch aNewDawn # Your branch is up-to-date with 'origin/aNewDawn'. # # Changes to be committed: #	modified:   bin/model.js #	modified:   package.json #

--- bin/model.js
+++ bin/model.js
@@ -1,61 +1,120 @@
 module.exports = function (dbpath, debugOutput) {
 	var debug = debugOutput || false;
 	var model = {};
+	var async = require('async');
 	var sqlite3 = require('sqlite3').verbose();
 	model.db = new sqlite3.Database(dbpath);
 
 	model.initDb = function() {
 		//TODO: init database structure
-		var stmts = [];
-		var s	 = "CREATE TABLE IF NOT EXISTS Clients (";
-		s = qB(s, "		id INT PRIMARY KEY,");
-		s = qB(s, "		name TEXT,");
-		s = qB(s, "		street1 TEXT,");
-		s = qB(s, "		street2 TEXT,");
-		s = qB(s, "		zip TEXT,");
-		s = qB(s, "		city TEXT,");
-		s = qB(s, "		short TEXT);");
-		stmts.push(s);
-		s = "CREATE UNIQUE INDEX clientShort ON Clients(short);";
-		stmts.push(s);
+		function createClients(callback) {
+			var cli	 = "CREATE TABLE IF NOT EXISTS Clients (";
+			cli = qB(cli, "		id INT PRIMARY KEY,");
+			cli = qB(cli, "		name TEXT,");
+			cli = qB(cli, "		street1 TEXT,");
+			cli = qB(cli, "		street2 TEXT,");
+			cli = qB(cli, "		zip TEXT,");
+			cli = qB(cli, "		city TEXT,");
+			cli = qB(cli, "		short TEXT UNIQUE);");
+			model.db.run(cli, [], function(err, result) {
+				if (err) {
+					debugOut("Error running query:\n" + cli + "\n\nErrors:\n" + err);
+				}
+				else
+				debugOut("Query successful!");
+				if (result)
+				debugOut("Results:\n" + result);
 
-		s = "CREATE TABLE IF NOT EXISTS Invoices (";
-		s = qB(s, "		id INT PRIMARY KEY,");
-		s = qB(s, "		date DATETIME,");
-		s = qB(s, "		ClientId INT,");
-		s = qB(s, "		FOREIGN KEY(fk_invoiceclient) REFERENCES Clients(id));");
-		stmts.push(s);
-
-		s = "CREATE TABLE IF NOT EXISTS InvoicePos (";
-		s = qB(s, "		id INT PRIMARY KEY,");
-		s = qB(s, "		title TEXT,");
-		s = qB(s, "		quantity NUMERIC,");
-		s = qB(s, "		value NUMERIC,");
-		s = qB(s, "		description TEXT,");
-		s = qB(s, "		fk_invoiceposinvoice INT,");
-		s = qB(s, "		FOREIGN KEY(fk_invoiceposinvoice) REFERENCE Invoice(id));");
-
-		s = "CREATE TABLE IF NOT EXISTS Times (";
-		s = qB(s, "		id INT PRIMARY KEY,");
-		s = qB(s, "		start DATETIME,");
-		s = qB(s, "		end DATETIME,");
-		s = qB(s, "		title TEXT,");
-		s = qB(s, "		description TEXT,");
-		s = qB(s, "		fk_timesclient INT,");
-		s = qB(s, "		fk_timesinvoicepos INT,");
-		s = qB(s, "		FOREIGN KEY(fk_timesclient) REFERENCES Clients(id),");
-		s = qB(s, "		FOREIGN KEY(fk_timesinvoicepos) REFERENCES InvoicePos(id));");
-		stmts.push(s);
-
-		debugOut("Running db init queries now!");
-		var count = 0;
-		stmts.forEach(function (stmt) {
-			count++;
-			var i=count;
-			debugOut("Running following query (" + i + ") now:\n" + stmt);
-			model.db.run(stmt, null, function(err) {
-				debugOut("Error running query (" + i + ")! Errors:\n" + JSON.stringify(err, null, 2));
+				callback(null, "Clients");
 			});
+		}
+
+		function createClientsIndex(callback) {
+			var cliU = "CREATE UNIQUE INDEX IF NOT EXISTS clientShort ON Clients(short);";
+			model.db.run(cliU, [], function(err, result) {
+				if (err)
+				debugOut("Error running query:\n" + cliU + "\n\nErrors:\n" + err);
+				else
+				debugOut("Query successful!");
+				if (result)
+				debugOut("Results:\n" + result);
+
+				callback();
+			});
+		}
+
+		function createInvoices(callback) {
+			var invoices = "CREATE TABLE IF NOT EXISTS Invoices (";
+			invoices = qB(invoices, "		id INT PRIMARY KEY,");
+			invoices = qB(invoices, "		date DATETIME,");
+			invoices = qB(invoices, "		ClientId INT,");
+			invoices = qB(invoices, "		fk_invoiceclient INT,");
+			invoices = qB(invoices, "		FOREIGN KEY(fk_invoiceclient) REFERENCES Clients(id));");
+			model.db.run(invoices, [], function(err, result) {
+				if (err)
+				debugOut("Error running query:\n" + invoices + "\n\nErrors:\n" + err);
+				else
+				debugOut("Query successful!");
+				if (result)
+				debugOut("Results:\n" + result);
+
+				callback();
+			});
+		}
+
+		function createInvoicePos(callback) {
+			var invoicepos = "CREATE TABLE IF NOT EXISTS InvoicePos (";
+			invoicepos = qB(invoicepos, "		id INT PRIMARY KEY,");
+			invoicepos = qB(invoicepos, "		title TEXT,");
+			invoicepos = qB(invoicepos, "		quantity NUMERIC,");
+			invoicepos = qB(invoicepos, "		value NUMERIC,");
+			invoicepos = qB(invoicepos, "		description TEXT,");
+			invoicepos = qB(invoicepos, "		fk_invoiceposinvoice INT,");
+			invoicepos = qB(invoicepos, "		FOREIGN KEY(fk_invoiceposinvoice) REFERENCES Invoice(id));");
+			model.db.run(invoicepos, [], function(err, result) {
+				if (err)
+				debugOut("Error running query:\n" + invoicepos + "\n\nErrors:\n" + err);
+				else
+				debugOut("Query successful!");
+				if (result)
+				debugOut("Results:\n" + result);
+
+				callback();
+			});
+		}
+
+		function createTimes(callback) {
+			var times = "CREATE TABLE IF NOT EXISTS Times (";
+			times = qB(times, "		id INT PRIMARY KEY,");
+			times = qB(times, "		start DATETIME,");
+			times = qB(times, "		end DATETIME,");
+			times = qB(times, "		title TEXT,");
+			times = qB(times, "		description TEXT,");
+			times = qB(times, "		fk_timesclient INT,");
+			times = qB(times, "		fk_timesinvoicepos INT,");
+			times = qB(times, "		FOREIGN KEY(fk_timesclient) REFERENCES Clients(id),");
+			times = qB(times, "		FOREIGN KEY(fk_timesinvoicepos) REFERENCES InvoicePos(id));");
+			model.db.run(times, [], function(err, result) {
+				if (err)
+				debugOut("Error running query:\n" + times + "\n\nErrors:\n" + err);
+				else
+				debugOut("Query successful!");
+				if (result)
+				debugOut("Results:\n" + result);
+
+				callback();
+			});
+		}
+
+		debugOut("Run all CREATE queries:\n");
+		async.series([
+			createClients,
+			createClientsIndex,
+			createInvoices,
+			createInvoicePos,
+			createTimes
+		], function(err, result) {
+			debugOut("Done.");
 		});
 	};
 
@@ -66,7 +125,7 @@ module.exports = function (dbpath, debugOutput) {
 
 	function debugOut(msg) {
 		if (debug)
-			console.log(msg);
+		console.log(msg);
 	}
 
 	return model;
