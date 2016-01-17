02ee07f39281ecadf1026900c195c26a409bdc67
Meik Specht <meik@schnitzelspecht.de>
2016-01-17 15:59:37 +0100

    refactored database structure

--- bin/model.js
+++ bin/model.js
@@ -24,13 +24,13 @@ module.exports = function (dbpath, debugoutput) {
 			var qry = new Qry();
 			qry.name = "Create: Clients";
 			qry.add("CREATE TABLE IF NOT EXISTS Clients (");
-			qry.add("		id INTEGER PRIMARY KEY AUTOINCREMENT,");
+			qry.add("		id INTEGER PRIMARY KEY,");
 			qry.add("		name TEXT,");
 			qry.add("		street1 TEXT,");
 			qry.add("		street2 TEXT,");
 			qry.add("		zip TEXT,");
 			qry.add("		city TEXT,");
-      qry.add("   email TEXT,");
+			qry.add("   email TEXT,");
 			qry.add("		short TEXT UNIQUE);");
 			db.run(qry.qry(), [], function(err, result) {
 				if (err) {
@@ -67,8 +67,8 @@ module.exports = function (dbpath, debugoutput) {
 			qry.add("CREATE TABLE IF NOT EXISTS Invoices (");
 			qry.add("		id INTEGER PRIMARY KEY,");
 			qry.add("		date DATETIME,");
-			qry.add("		fk_invoiceclient INTEGER,");
-			qry.add("		FOREIGN KEY(fk_invoiceclient) REFERENCES Clients(id));");
+			qry.add("		fk_Clients INTEGER,");
+			qry.add("		FOREIGN KEY(fk_Clients) REFERENCES Clients(id));");
 			db.run(qry.qry(), [], function(err, result) {
 				if (err)
 					debuglog("Error running query:\n" + qry.qry() + "\n\nErrors:\n" + err);
@@ -90,9 +90,10 @@ module.exports = function (dbpath, debugoutput) {
 			qry.add("		quantity NUMERIC,");
 			qry.add("		value NUMERIC,");
 			qry.add("		description TEXT,");
-      qry.add("   invoiced INTEGER DEFAULT 0,");
-			qry.add("		fk_invoiceposinvoice INTEGER,");
-			qry.add("		FOREIGN KEY(fk_invoiceposinvoice) REFERENCES Invoice(id));");
+			qry.add("		fk_Clients INTEGER,");
+			qry.add("		fk_Invoices INTEGER,");
+			qry.add("		FOREIGN KEY(fk_Clients) REFERENCES Clients(id)");
+			qry.add("		FOREIGN KEY(fk_Invoices) REFERENCES Invoices(id));");
 			db.run(qry.qry(), [], function(err, result) {
 				if (err)
 					debuglog("Error running query:\n" + qry.qry() + "\n\nErrors:\n" + err);
@@ -114,12 +115,11 @@ module.exports = function (dbpath, debugoutput) {
 			qry.add("		end DATETIME,");
 			qry.add("		title TEXT,");
 			qry.add("		description TEXT,");
-			qry.add("		invoiced INTEGER DEFAULT 0,");
 			qry.add("		archived INTEGER DEFAULT 0,");
-			qry.add("		fk_timesclient INTEGER,");
-			qry.add("		fk_timesinvoicepos INTEGER,");
-			qry.add("		FOREIGN KEY(fk_timesclient) REFERENCES Clients(id),");
-			qry.add("		FOREIGN KEY(fk_timesinvoicepos) REFERENCES InvoicePos(id));");
+			qry.add("		fk_Clients INTEGER,");
+			qry.add("		fk_InvoicePos INTEGER,");
+			qry.add("		FOREIGN KEY(fk_Clients) REFERENCES Clients(id),");
+			qry.add("		FOREIGN KEY(fk_InvoicePos) REFERENCES InvoicePos(id));");
 			db.run(qry.qry(), [], function(err, result) {
 				if (err)
 					debuglog("Error running query:\n" + qry.qry() + "\n\nErrors:\n" + err);
@@ -133,21 +133,21 @@ module.exports = function (dbpath, debugoutput) {
 		}
 
 		//TODO: is this really needed?
-    function createStaging(callback) {
-      var qry = new Qry();
-      qry.add("CREATE TABLE IF NOT EXISTS Staging (");
-      qry.add("   id INTEGER NOT NULL,").add("   type TEXT NOT NULL;");
-      db.run(qry.qry(), [], function (err, result) {
-        if (err)
-          debuglog("Error running query:\n" + times + "\n\nErrors:\n" + err);
-        else
-          debuglog("Query for Staging successful!");
-        if (result)
-          debuglog("Results:\n" + result);
+		function createStaging(callback) {
+			var qry = new Qry();
+			qry.add("CREATE TABLE IF NOT EXISTS Staging (");
+			qry.add("   id INTEGER NOT NULL,").add("   type TEXT NOT NULL;");
+			db.run(qry.qry(), [], function (err, result) {
+				if (err)
+					debuglog("Error running query:\n" + times + "\n\nErrors:\n" + err);
+				else
+					debuglog("Query for Staging successful!");
+				if (result)
+					debuglog("Results:\n" + result);
 
-        callback();
-      });
-    }
+				callback();
+			});
+		}
 
 		debuglog("Run all CREATE queries:\n");
 		async.series([
@@ -225,20 +225,20 @@ module.exports = function (dbpath, debugoutput) {
 	 */
 
 	model.Time = function (id, start, end, title, description, invoiced, archived, fk_timesclient, fk_timesinvoicepos) {
-    this.id = id;
-    this.start = start;
-    this.end = end;
-    this.title = title;
-    this.description = description;
-    this.invoiced = invoiced;
-    this.archived = archived;
-    this.fk_timesclient = fk_timesclient;
-    this.fk_timesinvoicepos = fk_timesinvoicepos;
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		this.title = title;
+		this.description = description;
+		this.invoiced = invoiced;
+		this.archived = archived;
+		this.fk_timesclient = fk_timesclient;
+		this.fk_timesinvoicepos = fk_timesinvoicepos;
 	}
 
 	model.Time.start = function (client, title, description, start, callback) {
-    if (!client || !title || !start)
-      throw "Wrong parameters!";
+		if (!client || !title || !start)
+			throw "Wrong parameters!";
 		var st = db.prepare("INSERT INTO Times (fk_timesclient, title, description, start) VALUES ($client, $title, $description, $start);");
 
 		if (client instanceof Array && client.length == 1)
@@ -246,7 +246,7 @@ module.exports = function (dbpath, debugoutput) {
 
 		var params = { $client: client.id, $title: title, $description: description, $start: start };
 		st.run(params, function(err) {
-      callback(err);
+			callback(err);
 		});
 	}
 
@@ -261,108 +261,108 @@ module.exports = function (dbpath, debugoutput) {
 				var start = new Date(row.start);
 				data.diffstr = moment.duration(moment().diff(start)).format('H [hours] m [minutes] s [seconds]');
 				data.diff = moment.duration(moment().diff(start));
-			  callback(err, data);
+				callback(err, data);
 			}
-      else {
-        callback (err, undefined);
-      }
+			else {
+				callback (err, undefined);
+			}
 		});
 	}
 
-  /**
-   * ends the most recent time tracking
-   */
-  model.Time.stop = function (end, callback) {
-    if (!end)
-      throw "Please supply an end date!";
+	/**
+	 * ends the most recent time tracking
+	 */
+	model.Time.stop = function (end, callback) {
+		if (!end)
+			throw "Please supply an end date!";
 
-    var qry = "update Times set end=$end where end is null";
-    var st = db.prepare(qry);
-    st.run({$end: end}, function(err) {
-      callback(err);
-    });
-  }
+		var qry = "update Times set end=$end where end is null";
+		var st = db.prepare(qry);
+		st.run({$end: end}, function(err) {
+			callback(err);
+		});
+	}
 
-  model.Time.list = function (filters, callback) {
-    if (filters && !(filters instanceof Array))
-      throw "filters parameter must be an array or null";
+	model.Time.list = function (filters, callback) {
+		if (filters && !(filters instanceof Array))
+			throw "filters parameter must be an array or null";
 
-    // base query
-    var qry = "select t.*, c.id clientid, c.name clientname, c.short shortKey from Times t JOIN Clients c ON t.fk_timesclient=c.id";
+		// base query
+		var qry = "select t.*, c.id clientid, c.name clientname, c.short shortKey from Times t JOIN Clients c ON t.fk_timesclient=c.id";
 
-    // process filters
-    var params = {};
-    if (filters) {
-      qry += " where ";
-      var junctor = " AND ";
-      for (var i=0; i<filters.length; i++) {
-        var item = filters[i];
-        if (i>=1)
-          qry += junctor;
+		// process filters
+		var params = {};
+		if (filters) {
+			qry += " where ";
+			var junctor = " AND ";
+			for (var i=0; i<filters.length; i++) {
+				var item = filters[i];
+				if (i>=1)
+					qry += junctor;
 
-        if (item.length == 2) {
-          qry += item[0] + "=$" + item[0];
-        }
-        else if (item.length == 3) {
-          qry += item[0] + " " + item[2] + " $" + item[0];
-        }
-        else {
-          qry += ""+item[0];
-          continue;
-        }
+				if (item.length == 2) {
+					qry += item[0] + "=$" + item[0];
+				}
+				else if (item.length == 3) {
+					qry += item[0] + " " + item[2] + " $" + item[0];
+				}
+				else {
+					qry += ""+item[0];
+					continue;
+				}
 
-        // this builds the params object which will be used
-        // in the parameterized query
-        params['$'+item[0]] = item[1];
-      }
-    }
+				// this builds the params object which will be used
+				// in the parameterized query
+				params['$'+item[0]] = item[1];
+			}
+		}
 
-    // run query
-    debuglog("running query: " + qry);
-    var st = db.prepare(qry);
-    st.all(params, function (err, rows) {
-      for (var i=0; i<rows.length; i++) {
-        var row = rows[i];
+		// run query
+		debuglog("running query: " + qry);
+		var st = db.prepare(qry);
+		st.all(params, function (err, rows) {
+			for (var i=0; i<rows.length; i++) {
+				var row = rows[i];
 
-        // calc duration
-        var start = new Date(row.start);
-        var end = new Date(row.end);
+				// calc duration
+				var start = new Date(row.start);
+				var end = new Date(row.end);
 				row.diffstr = moment.duration(moment(end).diff(start)).format('H [hours] m [minutes] s [seconds]');
 				row.diff = moment.duration(moment(end).diff(start));
-      }
+			}
 
-      callback(err, rows);
-    });
-  };
+			callback(err, rows);
+		});
+	};
 
-  model.InvoicePos = function () {
+	model.InvoicePos = function () {
 
-  }
+	}
 
-  model.InvoicePos.create = function (times) {
-    if (times.constructor !== Array || times.length == 0)
-       throw "times parameter must be an array of Time IDs or model.Time instances!";
+	model.InvoicePos.create = function (times) {
+		if (times.constructor !== Array || times.length == 0)
+			throw "times parameter must be an array of Time IDs or model.Time instances!";
 
-    // get all time records from database
-    var tobjs = [];         // list of time objects
-    var qry = "select * from times where id=$id";
-    db.serialize(function() { // need to serialize database queries!
-      for (var i=0; i<times.length; i++) {
-        if (times[i] instanceof model.Time) {
-          tobjs.push(times[i]);
-        }
-        else {
-          var st = db.prepare(qry);
-          st.get([{"$id": times[i]}], function (err, row) {
-            if (err)
-              throw "There was an error retreiving all time records from the database! ID: " + times[i];
+		// get all time records from database
+		var tobjs = [];         // list of time objects
+		var qry = "select * from times where id=$id";
+		db.serialize(function() { // need to serialize database queries!
+			for (var i=0; i<times.length; i++) {
+				if (times[i] instanceof model.Time) {
+					tobjs.push(times[i]);
+				}
+				else {
+					var st = db.prepare(qry);
+					st.get([{"$id": times[i]}], function (err, row) {
+						if (err)
+							throw "There was an error retreiving all time records from the database! ID: " + times[i];
 
-            tobjs.push(row);
-          });
-        }
-      }
-    });
-  }
+						tobjs.push(row);
+					});
+				}
+			}
+		});
+	}
 
 	/*
 	 * ######## MODELS END #########
@@ -376,18 +376,18 @@ module.exports = function (dbpath, debugoutput) {
 		return str;
 	}
 
-  function Qry() {
-    this._qry = "";
-  }
+	function Qry() {
+		this._qry = "";
+	}
 
-  Qry.prototype.add = function (string) {
-    this._qry = this._qry + "\n" + string;
-    return this;
-  }
+	Qry.prototype.add = function (string) {
+		this._qry = this._qry + "\n" + string;
+		return this;
+	}
 
-  Qry.prototype.qry = function () {
-    return this._qry;
-  }
+	Qry.prototype.qry = function () {
+		return this._qry;
+	}
 
 	return model;
 }
