6cc9342dac726063fcaf294b8a150990fa0bbe5d
Meik Specht <meik@schnitzelspecht.de>
2016-01-20 23:19:34 +0100

    refactored asTable: now takes an object with rows, keys and options

--- bin/util.js
+++ bin/util.js
@@ -48,7 +48,7 @@ module.exports = function() {
 		}
 
 		// print table and get selection from user
-		asTable.apply(this, [list]);
+		asTable({ rows: list });
 		var answer = rls.question(options.question);
 		answer = answer.replace(/ /g, "");
 		ids = answer.split(",");
@@ -81,47 +81,61 @@ module.exports = function() {
 		}
 	}
 
-	utils.asTable = function () {
+	utils.asTable = function (data) {
 		// arguments:
-		var objectArray = (arguments[0].constructor === Array) ? arguments[0] : arguments;
-		var keyList = Array.prototype.slice.call(arguments, 1);
+		// data.rows: an array of objects that need to be displayed
+		// data.keyList: object of keys of those objects that should be displayed
+		//		example keyList obj:
+		//		keyList = {
+		//			name: {
+		//				alias: "Name",
+		//				stripNull: true,
+		//				format: string,
+		//				align: right
+		//			}
+		//		}
+		// data.options: TODO
+		var dataRows = data.rows;
+		var keyList = data.keyList || null;
+		var options = data.options || null; // not yet implemented - will hold global options for all columns
 
-		var keys = [];       // contains all key names of objects stored in objectArray
-		var keydict = [];
 		var maxlength = []; // max length for every column
-		var formats = [];
 
 		// validate arguments: objectArray
-		if (!objectArray) {
+		if (!dataRows) {
 			throw {"name": "ArgumentInvalidEx", "msg": "You must supply at least one object to print as table"};
 		}
 
 		// validate arguments: keys
+		// ... and initialize keyList
 		if (!keyList || keyList.length === 0 || (keyList.length === 1 && keyList[0] === "*")) {
-			for (var key in objectArray[0]) {
-				if (typeof key !== 'function') keyList.push(key);
+			keyList = []; // get rid of "*" element 
+			for (var key in dataRows[0]) { // assumes that every object has the same keys
+				if (key.constructor !== Function) 
+					keyList.push({ name: key, alias: key });
 			}
 		}
 
-		// create keys array and keydict to save key aliases for the table
 		// also initializes maxlength[key] with the length of the keyname which will be used as header
-		keyList.forEach(function(key) {
-			if (key.constructor === Array) {
-				keys.push(key[0]);
-				keydict[key[0]] = key[1];
-				maxlength[key[0]] = String(key[1]).length;
+		keyList.forEach(function(key, index) {
+			if (key.constructor === String) {
+				key = { name: key, alias: key };
+				keyList[index] = key; // i want all keys in keyList to be objects for easier use
 			}
-			else {
-				keys.push(key);
-				maxlength[key] = String(key).length;
-			}
+
+			// set default values
+			keyList[index].stripNull = keyList[index].stripNull || true;
+			keyList[index].format = keyList[index].format || "s";
+			keyList[index].align = keyList[index].align || "left";
+
+			maxlength[key.name] = String(key.alias).length;
 		});
 
 		// find max length of content for each key
-		objectArray.forEach(function (obj) {
-			keys.forEach(function (key) {
-				if (String(obj[key]).length > maxlength[key]) {
-					maxlength[key] = String(obj[key]).length;
+		dataRows.forEach(function (row) {
+			keyList.forEach( function (key) {
+				if (String(row[key.name]).length > maxlength[key.name]) {
+					maxlength[key.name] = String(row[key.name]).length;
 				}
 			});
 		});
@@ -129,24 +143,37 @@ module.exports = function() {
 		// print header
 		var line = "";
 		var headerLabels = [];
-		keys.forEach(function(key) {
-			line += "%-" + maxlength[key] + "s|";   // insert max length of key/value for obj key
-			headerLabels.push((keydict[key]) ? keydict[key] : key);   // save headers in array
+		keyList.forEach(function(key) {
+			line += "%-" + maxlength[key.name] + "s|";   // insert max length of key/value for obj key
+			headerLabels.push(key.alias);   // save headers in array
 		});
 		line = line.substring(0,line.length-1); // remove last "|", line done here
 
 		var ar = headerLabels;
 		ar.unshift(line);               // put the format string in front of args array
 		console.log(printf.apply(this, ar)); // print headers
+
 		// print content
-		objectArray.map(function (obj) { // loop objects
-			var objvalues = [];
-			keys.forEach(function (key) {
-				objvalues.push(obj[key]); // push object values into array for each key
+		// but first rebuild the format line
+		line = "";
+		keyList.forEach(function (key) {
+			line += "%";
+			if (key.align === "left") line += "-";
+			line += maxlength[key.name];
+			line += key.format;
+			line += "|";
+		});
+		line = line.substring(0, line.length -1);
+		dataRows.forEach(function (row) { // loop objects
+			var rowValues = [];
+			keyList.forEach(function (key) {
+				// convert null to empty string if configured
+				var value = (key.stripNull) ? stripNull(row[key.name]) : row[key.name];
+				rowValues.push(value); // push object values into array for each key
 			});
 
-			objvalues.unshift(line);
-			console.log(printf.apply(this, objvalues));
+			rowValues.unshift(line);
+			console.log(printf.apply(this, rowValues));
 		});
 	}
 
